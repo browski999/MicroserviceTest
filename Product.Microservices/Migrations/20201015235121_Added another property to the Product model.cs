6564087362eb8f6d814fc0b4f169b2e6c2e4e1c0
Dell <browski999@gmail.com>
2020-10-16 00:53:33 +0100

    Initial commit

---
+++ Product.Microservices/Migrations/20201015235121_Added another property to the Product model.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Product.Microservices.Migrations
+{
+    public partial class AddedanotherpropertytotheProductmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Old",
+                table: "Products",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Old",
+                table: "Products");
+        }
+    }
+}
