e5e750a61cd1936ee01886ca2f5d89744bc9a1b2
Dell <browski999@gmail.com>
2020-11-04 13:37:36 +0000

    test

--- Product.Microservices/Startup.cs
+++ Product.Microservices/Startup.cs
@@ -20,7 +20,7 @@ namespace Product.Microservices
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-
+        // Last test again
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ProductDbContext>(options =>
