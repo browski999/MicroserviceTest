64c56124c0c35dba996b9ed9ca0a6507cf906eb9
Dell <browski999@gmail.com>
2020-11-04 17:34:23 +0000

    Test

--- Product.Microservices/Controllers/ProductController.cs
+++ Product.Microservices/Controllers/ProductController.cs
@@ -1,8 +1,4 @@
-﻿using MediatR;
-using Microsoft.AspNetCore.Mvc;
-using Product.Microservices.Features.ProductFeatures.Commands;
-using Product.Microservices.Features.ProductFeatures.Queries;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,46 +8,51 @@ namespace Product.Microservices.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public ProductController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Create(CreateProductCommand command)
-        {
-            return Ok(await _mediator.Send(command));
-        }
-
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public ActionResult GetAll()
         {
-            return Ok(await _mediator.Send(new GetAllProductsQuery()));
+            return Content("BOB");
         }
+        //private readonly IMediator _mediator;
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            return Ok(await _mediator.Send(new GetProductByIdQuery { Id = id }));
-        }
+        //public ProductController(IMediator mediator)
+        //{
+        //    _mediator = mediator;
+        //}
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            return Ok(await _mediator.Send(new DeleteProductByIdCommand { Id = id }));
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> Create(CreateProductCommand command)
+        //{
+        //    return Ok(await _mediator.Send(command));
+        //}
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
-        {
-            if (id != command.Id)
-            {
-                return BadRequest(typeof(UpdateProductCommand));
-            }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAll()
+        //{
+        //    return Ok(await _mediator.Send(new GetAllProductsQuery()));
+        //}
 
-            return Ok(await _mediator.Send(command));
-        }
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetById(int id)
+        //{
+        //    return Ok(await _mediator.Send(new GetProductByIdQuery { Id = id }));
+        //}
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    return Ok(await _mediator.Send(new DeleteProductByIdCommand { Id = id }));
+        //}
+
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        //{
+        //    if (id != command.Id)
+        //    {
+        //        return BadRequest(typeof(UpdateProductCommand));
+        //    }
+
+        //    return Ok(await _mediator.Send(command));
+        //}
     }
 }
