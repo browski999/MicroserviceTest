62d678d21d2d4ef5a496f31f59bdc99cfb0ccae7
Dell <browski999@gmail.com>
2020-11-04 18:06:49 +0000

    Removed dependency on Mediatr

--- Product.Microservices/Controllers/ProductController.cs
+++ Product.Microservices/Controllers/ProductController.cs
@@ -1,6 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Product.Microservices.Features.ProductFeatures.Queries;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -25,9 +25,74 @@ namespace Product.Microservices.Controllers
         //}
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<List<Models.Product>> GetAll()
         {
-            return Ok(await _mediator.Send(new GetAllProductsQuery()));
+            //return Ok(await _mediator.Send(new GetAllProductsQuery()));
+            var productList = new List<Models.Product>
+                {
+                    new Models.Product
+                    {
+                        Id = 1,
+                        Barcode = "Test string",
+                        ConfidentialData = "Some confidential data",
+                        BuyingPrice = 12.95m,
+                        Description = "This is a test description",
+                        IsActive = false,
+                        Name = "One product",
+                        Rate = 15.0m,
+                        SKU = "test sku"
+                    },
+                    new Models.Product
+                    {
+                        Id = 2,
+                        Barcode = "Test string 1",
+                        ConfidentialData = "Some confidential data with another",
+                        BuyingPrice = 2.95m,
+                        Description = "This is a test description",
+                        IsActive = false,
+                        Name = "One product",
+                        Rate = 15.0m,
+                        SKU = "test sku"
+                    },
+                    new Models.Product
+                    {
+                        Id = 3,
+                        Barcode = "Test string",
+                        ConfidentialData = "Some confidential data",
+                        BuyingPrice = 12.95m,
+                        Description = "This is a test description",
+                        IsActive = false,
+                        Name = "One product",
+                        Rate = 15.0m,
+                        SKU = "test sku"
+                    },
+                    new Models.Product
+                    {
+                        Id = 4,
+                        Barcode = "Test string",
+                        ConfidentialData = "Some confidential data",
+                        BuyingPrice = 12.95m,
+                        Description = "This is a test description",
+                        IsActive = false,
+                        Name = "One product",
+                        Rate = 15.0m,
+                        SKU = "test sku"
+                    },
+                    new Models.Product
+                    {
+                        Id = 5,
+                        Barcode = "Test string",
+                        ConfidentialData = "Some confidential data",
+                        BuyingPrice = 12.95m,
+                        Description = "This is a test description",
+                        IsActive = false,
+                        Name = "One product",
+                        Rate = 15.0m,
+                        SKU = "test sku"
+                    },
+                };
+
+            return productList;
         }
 
         //[HttpGet("{id}")]
