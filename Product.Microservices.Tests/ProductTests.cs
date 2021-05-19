a82b4648b1e1c8787ccb56e8211cfcd0d79fde4d
Dell <browski999@gmail.com>
2021-05-19 11:27:57 +0100

    Added another test

--- Product.Microservices.Tests/ProductTests.cs
+++ Product.Microservices.Tests/ProductTests.cs
@@ -1,6 +1,7 @@
 ﻿using Moq;
 using Product.Microservices.Features.ProductFeatures.Queries;
 using System.Linq;
+using System.Threading;
 using Xunit;
 
 namespace Product.Microservices.Tests
@@ -17,12 +18,29 @@ namespace Product.Microservices.Tests
             GetAllProductsQueryHandler handler = new GetAllProductsQueryHandler(context.Object);
 
             //Act
-            var result = await handler.Handle(query, new System.Threading.CancellationToken());
+            var result = await handler.Handle(query, new CancellationToken());
 
             //Assert
             Assert.True(result.Count() > 1);
             Assert.False(result.Count() < 1);
 
+        }
+
+        [Fact]
+        public async void ShouldReturnProductById()
+        {
+            //Arrange
+            var context = new Mock<IProductDbContext>();
+
+            GetProductByIdQuery query = new GetProductByIdQuery();
+            GetProductByIdQueryHandler handler = new GetProductByIdQueryHandler(context.Object);
+
+            //Act
+            var result = await handler.Handle(query, new CancellationToken());
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.True(result.Id > 0);
         }
 
     }
